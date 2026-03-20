60d95042bd4a626e4857d2c9c1962a68f4bfc2f0
Anthony Elorm Zottor <aezottor@gmail.com>
2026-03-20 13:49:53 +0300

    feat: complete Day 3 with VPC fix and architecture docs

--- Day-3/main.tf
+++ Day-3/main.tf
@@ -1,56 +1,68 @@
 provider "aws" {
-    region = "us-east-1"
+  region = "us-east-1"
 }
 
+# 1. Automatically find your Default VPC
+data "aws_vpc" "default" {
+  default = true
+}
+
+# 2. Security Group (The "Bouncer")
 resource "aws_security_group" "web-sg" {
-    name       = "web-sg"
-    description = "Allow HTTP traffic"
+  name        = "web-server-sg"
+  description = "Allow HTTP inbound traffic"
+  vpc_id      = data.aws_vpc.default.id
 
-    ingress {
-        from_port   = 80
-        to_port     = 80
-        protocol    = "tcp"
-        cidr_blocks = ["0.0.0.0/0"]
-    }
+  ingress {
+    from_port   = 80
+    to_port     = 80
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  egress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
 }
 
-    data "aws_ami" "amazon_linux" {
-    most_recent = true
-    owners      = ["amazon"]
+# 3. Data Source for the latest Amazon Linux 2023 AMI
+data "aws_ami" "amazon_linux" {
+  most_recent = true
+  owners      = ["amazon"]
+  filter {
+    name   = "name"
+    values = ["al2023-ami-*-x86_64"]
+  }
+}
 
-    filter {
-        name   = "name"
-        values = ["al2023-ami-*-x86_64"]
-    }
-
-    filter {
-        name   = "virtualization-type"
-        values = ["hvm"]
-    }
-    }
-
-resource "aws_instance" "Web-Server" {
-    ami           = data.aws_ami.amazon_linux.id # Amazon Linux 2023 AMI ID
-    instance_type = "t2.micro"
-    security_groups = [aws_security_group.web-sg.name]
-    vpc_security_group_ids = [aws_security_group.web-sg.id]
-
-    tags = {
-        Name = "Terraform-Web-Server"
-    }
+# 4. My Web Server 
+resource "aws_instance" "web-server" {
+  ami           = data.aws_ami.amazon_linux.id
+  instance_type = "t3.micro"
   
-}
+  # Using the Security Group ID is best practice
+  vpc_security_group_ids = [aws_security_group.web-sg.id]
 
-user_data = <<-EOF
-    #!/bin/bash
-    dnf update -y
-    dnf install -y httpd
-    systemctl start httpd
-    systemctl enable httpd
-
-    echo "<h1>Hello from 30 Days Terraform Challenge 🚀</h1>" > /var/www/html/index.html
-    EOF
+  # This script runs on boot to install Apache and create your page
+  user_data = <<-EOF
+              #!/bin/bash
+              dnf update -y
+              dnf install -y httpd
+              systemctl start httpd
+              systemctl enable httpd
+              echo "<h1>Hello from 30 Days Terraform Challenge 🚀</h1>" > /var/www/html/index.html
+              EOF
 
   tags = {
     Name = "Terraform-Web-Server"
   }
+}
+
+# 5. Output the Public IP so you can test it immediately
+output "ec2_instance_ip" {
+  description = "Public IP of the web server"
+  value = aws_instance.web-server.public_ip
+}
